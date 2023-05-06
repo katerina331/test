a2e85c8536a04ad18b3ce980bfb5a9e182f3e095
Ekaterina <katerina331-441@ya.ru>
2023-05-06 18:08:42 +0300

    Initial commit

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -21,7 +21,6 @@ class CallbackTest {
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
