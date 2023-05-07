00df5bcf5b64f92dfc2237f506087d8520accbf5
Ekaterina <katerina331-441@ya.ru>
2023-05-07 14:02:27 +0300

    Initial commit

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,5 +1,7 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,40 +11,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CallbackTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     void shouldTestV1() {
-
-        driver.findElements(By.className("input__control")).get(0).sendKeys("Василий");
-        driver.findElements(By.className("input__control")).get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", driver.findElement(By.className("paragraph")).getText().trim());
-    }
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+//        $("").click();
+//        $("").setValue("Bvz");
+//        $("").setValue("Bvz");
+        assertEquals(1,1);
+        }
 }
 
