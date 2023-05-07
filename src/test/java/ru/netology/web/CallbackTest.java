59ba3d8c2dbc3e7913cf763b3b16bdf17dcd7031
Ekaterina <katerina331-441@ya.ru>
2023-05-07 14:15:38 +0300

    Initial commit

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,18 +1,7 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,10 +11,7 @@ class CallbackTest {
     void shouldTestV1() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
-//        $("").click();
-//        $("").setValue("Bvz");
-//        $("").setValue("Bvz");
-        assertEquals(1,1);
-        }
+        assertEquals(1, 1);
+    }
 }
 
