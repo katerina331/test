21a7c54fe4c04ad60bf61a168b8fc5c50c4855a2
Ekaterina <katerina331-441@ya.ru>
2023-05-08 23:04:45 +0300

    Initial commit

--- src/test/java/ru/netology/web/WebTest.java
+++ src/test/java/ru/netology/web/WebTest.java
@@ -2,8 +2,18 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Condition.appear;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,9 +21,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WebTest {
     @Test
     void shouldTestV1() {
-        Configuration.holdBrowserOpen = true;
-        open("localhost:9999");
-  //      $("[type=tel]").should(appear);
-        assertEquals(1, 1);
+        Configuration.holdBrowserOpen = false;
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Воронеж");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String date = dateFormat.format(Date.valueOf(LocalDate.now().plusDays(4)));
+        $("[data-test-id=date] input").doubleClick().sendKeys(date);
+        $("[data-test-id=name] input").setValue("Васильев-Иванов Василий");
+        $("[data-test-id=phone] input").setValue("+78002225577");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=notification]").should(visible, Duration.ofSeconds(15));
     }
 }
